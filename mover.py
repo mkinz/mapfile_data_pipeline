0e1ba1fbb2e2cf0387d31277ead293137a594edf
Matt <mkinz@users.noreply.github.com>
2021-04-15 17:58:41 -0400

    Update mover.py
    refactoring pathnames

--- mover.py
+++ mover.py
@@ -1,7 +1,11 @@
+# RUNS ON BTVM sfc9mhtvp01
+
 import shutil
 import os
 import datetime
 import sys
+import glob
+
 
 class Mover:
 
@@ -11,14 +15,15 @@ class Mover:
         except FileNotFoundError:
             print("File not found, exiting.")
 
-    def backup_current_oasis_file(self, dst, bkup_dirname, filename):
+    def backup_current_file(self, dst, bkup_path, filename):
         try:
             bkup_date = datetime.datetime.today().strftime('%Y-%m-%d-%s')
             shutil.move(os.path.join(dst, filename),
-                    os.path.join(dst, bkup_dirname, filename+"."+bkup_date))
+                        os.path.join(bkup_path, filename + "." + bkup_date))
         except FileNotFoundError:
             print(f"File not found in path \n{dst}\nExiting")
             sys.exit()
+
 
 class Checker:
 
@@ -35,10 +40,10 @@ class Checker:
 
 
 class Runner:
-
-    source_path = '/users/home/mkinzler/scripts/utilities/test_source'
-    destination_path = '/users/home/mkinzler/scripts/utilities/test_dest'
-    bkup_dirname = "oasis_mapfile_backups"
+    incoming_file_path = '/users/home/mkinzler/scripts/utilities/oasisMerger'
+    prod_mapfile_path = '/users/home/mkinzler/scripts/utilities/test_dest'
+    backup_directory = "oasis_mapfile_backups"
+    backup_path = os.path.join(prod_mapfile_path, backup_directory)
     mapfile = 'red_oasis_map'
 
     def __init__(self, checker, mover):
@@ -46,26 +51,30 @@ class Runner:
         self.mover = mover
 
     def run_it(self):
-
-        #instantiate objects
-        myChecker = Checker()
-        myMover = Mover()
+        # instantiate objects
+        my_checker = Checker()
+        my_mover = Mover()
 
         # first, check if new mapfile exists in source path, else exit
-        myChecker.check_if_new_mapfile_exists(self.source_path, self.mapfile)
+        my_checker.check_if_new_mapfile_exists(self.incoming_file_path, self.mapfile)
 
         # if it does exist, then make backup of the old mapfile
-        myChecker.make_backup_dir_if_not_exists(self.destination_path, self.bkup_dirname)
-        myMover.backup_current_oasis_file(self.destination_path, self.bkup_dirname, self.mapfile)
+        my_checker.make_backup_dir_if_not_exists(self.prod_mapfile_path, self.backup_directory)
+        my_mover.backup_current_file(self.prod_mapfile_path, self.backup_path, self.mapfile)
 
         # move new mapfile from src to dest (prod) location
-        myMover.move_file(self.source_path, self.destination_path, self.mapfile)
+        my_mover.move_file(self.incoming_file_path, self.prod_mapfile_path, self.mapfile)
+
+        # move logfile to backup destination path
+        for logfile in glob.glob('*log*'):
+            my_mover.backup_current_file(self.incoming_file_path, self.backup_path, logfile)
+
 
 def main():
-    myRunner = Runner(Checker(), Mover())
-    myRunner.run_it()
+    my_runner = Runner(Checker(), Mover())
+    my_runner.run_it()
+
 
 if __name__ == '__main__':
     main()
-
-
+# everything works
