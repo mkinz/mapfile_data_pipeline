ca5540487a4d8390e5c62b081af2e80320731f2f
Matt <mkinz@users.noreply.github.com>
2021-04-15 13:36:37 -0400

    Update server_B_runner.sh
    updating paths

--- server_B_runner.sh
+++ server_B_runner.sh
@@ -6,7 +6,9 @@ spawn sftp mkinzler@serverA:PycharmProjects/filemover
 expect "Password:"
 send "*********************"
 expect "sftp>"
-send "get addendum_file\n"
+send "get main_file\n"
+expect "sftp>"
+send "get log_file\n"
 expect "sftp>"
 send "bye\n"
 EOS
