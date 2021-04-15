26c246a38d0d7d522eaf5eb8412805eaf1235215
Matt <mkinz@users.noreply.github.com>
2021-04-15 14:15:14 -0400

    Update test_transformer.py
    pep8 refactor

--- test_transformer.py
+++ test_transformer.py
@@ -1,8 +1,9 @@
 from pandas import DataFrame
 
-from transformer import Cleaner
-from transformer import Joiner
-from transformer import Logger
+from oasis_mapfile_transformer import Cleaner
+from oasis_mapfile_transformer import Joiner
+from oasis_mapfile_transformer import Logger
+
 
 # Test Cleaner
 
@@ -11,23 +12,27 @@ def test_remove_hashes():
     dut = Cleaner().remove_hashes(list_with_hashes)
     assert dut == ['eggs']
 
+
 def test_filter_longer_than_30_chars():
     list_of_long_strings = ['asdfasdfasdfasdfasdfasdfasdfasdfa', 'abc']
     dut = Cleaner().filter_longer_than_30_chars(list_of_long_strings)
     assert dut == ['abc']
+
 
 def test_filter_empty_lines_in_list():
     list_of_stuff_with_blanks = ['a', 'b', '', 'c']
     dut = Cleaner().filter_blank_lines(list_of_stuff_with_blanks)
     assert dut == ['a', 'b', 'c']
 
+
 def test_filter_many_space_empty_lines_in_list():
     list_of_stuff_with_many_space_blanks = ['a', 'b', '          ', 'c']
     dut = Cleaner().filter_blank_lines(list_of_stuff_with_many_space_blanks)
     assert dut == ['a', 'b', 'c']
 
+
 def test_make_dataframe():
-    list_of_stuff_for_dataframe = ['a','b','c']
+    list_of_stuff_for_dataframe = ['a', 'b', 'c']
     dut = Cleaner().make_dataframe(list_of_stuff_for_dataframe)
     assert isinstance(dut, DataFrame)
 
@@ -35,11 +40,12 @@ def test_make_dataframe():
 # Test Joiner
 
 def test_join_dataframes_together():
-    df1 = ['a','b']
-    df2 = ['c','d']
+    df1 = ['a', 'b']
+    df2 = ['c', 'd']
     df3 = df1.append(df2)
-    dut = Joiner().join_files(df1,df2)
+    dut = Joiner().join_files(df1, df2)
     assert dut == df3
+
 
 # Test Logger
 
@@ -47,10 +53,12 @@ def test_create_logger_header():
     dut = Logger().build_logger_header()
     assert isinstance(dut, list)
 
+
 def test_create_logger_contents():
     sample_contents = ['Anchored_OPC_Final_lessthan_MINSPACE                    10000  201']
     dut = Logger().build_logger_contents_of_removed_items(sample_contents)
     assert dut == sample_contents
+
 
 def test_create_logger_in_memory():
     sample_header = ['Test']
